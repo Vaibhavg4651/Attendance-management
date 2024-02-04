1f7ec1e2f8f335d37ee9ae8a381260b3f90bd744
Kshitiz Sharma <kshitiz01sharma@gmail.com>
2024-02-04 19:11:20 +0530

    UI Update

---
+++ Frontend/src/store.js
@@ -0,0 +1,8 @@
+// import React, { useReducer } from 'react'
+// import { configureStore } from '@reduxjs/toolkit';
+// const store=configureStore({
+//     reducer:{
+//         user:useReducer
+//     }
+// })
+// export default store;
